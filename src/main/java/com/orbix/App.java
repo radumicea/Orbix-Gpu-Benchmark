6eb84ed03824b9e99993eb10114c33129cb2975b
Nilgesz Timea <timea.nilgesz@student.upt.ro>
2022-04-12 18:51:06 +0300

    Added Aparapi dependency

--- src/main/java/com/orbix/App.java
+++ src/main/java/com/orbix/App.java
@@ -1,5 +1,8 @@
 package com.orbix;
 
+import com.aparapi.Kernel;
+import com.aparapi.Range;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final float inA[] = { 1.2F, 2.3F, 1F, 8F, 11.5F };
+        final float inB[] = { 2.2F, 8.3F, 9F, 10F, 2.444443F };
+        assert (inA.length == inB.length);
+        final float result[] = new float[inA.length];
+
+        Kernel kernel = new Kernel() {
+            @Override
+            public void run() {
+                int i = getGlobalId();
+                result[i] = inA[i] + inB[i];
+            }
+        };
+
+        Range range = Range.create(result.length);
+        kernel.execute(range);
+
+        for (float f : result)
+        {
+            System.out.println(f);
+        }
     }
 }
