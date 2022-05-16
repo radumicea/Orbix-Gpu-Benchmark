29a335e607a2dcb8282a4cb85b0b563ca68ebf8d
Radu Micea <radumicea4@gmail.com>
2022-05-16 23:10:23 +0300

    Merged

--- src/main/java/com/orbix/database/CreateConnection.java
+++ src/main/java/com/orbix/database/CreateConnection.java
@@ -1,21 +1,24 @@
 package com.orbix.database;
 
 import static com.mongodb.client.model.Filters.eq;
-import org.bson.Document;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class CreateConnection {
-    public static void main( String[] args ) {
-        // Replace the uri string with your MongoDB deployment's connection string
-        String uri = "mongodb+srv://orbixMember:orbixTeam@cluster0.fma2x.mongodb.net/test";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("ORBIX");
-            MongoCollection<Document> collection = database.getCollection("records");
-            Document doc = collection.find(eq("User", "Simion Inisconi")).first();
-            System.out.println(doc.toJson());
-        }
+
+  public static void main(String[] args) {
+    // Replace the uri string with your MongoDB deployment's connection string
+    String uri =
+      "mongodb+srv://orbixMember:orbixTeam@cluster0.fma2x.mongodb.net/test";
+    try (MongoClient mongoClient = MongoClients.create(uri)) {
+      MongoDatabase database = mongoClient.getDatabase("ORBIX");
+      MongoCollection<Document> collection = database.getCollection("records");
+      Document doc = collection.find(eq("User", "Simion Inisconi")).first();
+      System.out.println(doc.toJson());
     }
+  }
 }
